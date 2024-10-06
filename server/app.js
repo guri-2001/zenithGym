5c8c610e942b72839c1afa0004156b0915b3ee07
Gurbir Singh <123073042+guri-2001@users.noreply.github.com>
2024-10-06 23:48:47 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(express.json());
 const crossOptions = {
-    origin: "https://zenith-gym.vercel.app,
+    origin: "https://zenith-gym.vercel.app",
     credentials: true
 }
 app.use(cors(crossOptions))
